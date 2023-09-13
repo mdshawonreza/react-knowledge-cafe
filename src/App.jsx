ae175ec538c265071fdddbac86e1d1464921b414
Md Shawon Reza <fahadshawon241@gmail.com>
2023-09-13 21:06:45 +0600

    add this new projects

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import './App.css'
+import Blogs from './component/Blogs/Blogs'
+import Header from './component/Header/Header'
+
+function App() {
+
+
+  return (
+    <>
+      <Header></Header>
+      <Blogs></Blogs>
+       
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,12 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-3xl text-white font-bold bg-orange-500 p-4 text-center border-b-4 border-b-gray-500'>React Knowledge Cafe</h1>
+
+        </div>
+    );
+};
+
+export default Header;
